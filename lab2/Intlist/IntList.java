00a3e4b5eb6d7383c3f4b1aec7031c997b114846
Hannah Wu <dove.fry@gmail.com>
2022-01-19 22:42:34 -0500

    completed lab 2 v2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,6 +83,9 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
         IntList temp = A;
+        if (A==null){
+            A = B;
+        }
         while (A.rest!=null){
             A=A.rest;
         }
